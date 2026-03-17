3e85c9ef815bf3aeda44fee2e405be209d9f7a50
wmarbar <wmartinez0.01666@gmail.com>
2026-03-16 20:37:27 -0500

    data stellar labs sas

--- app.py
+++ app.py
@@ -7,34 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/quienesSomos')
-def quienesSomos():
+@app.route('/quienes-somos')
+def quienes_somos():
     return render_template('quienesSomos.html')
 
-
-@app.route('/resenas')
-def resenas():
-    return render_template('resenas.html')  # reseñas.html
-
-@app.route('/demos')
-def demos():
-    return render_template('demos.html')
-
-
-@app.route('/rompe-ladrillos')
-def rompe_ladrillos():
-    return render_template('rompeladrillos.html')
-
-
-@app.route('/descargar-demo')
-def descargar_demo():
-    # Cambia la ruta al archivo correcto que pusiste en tu carpeta static
-    return send_file("static/ST_QuickExchange_demo.rar", as_attachment=True)
-
-
-@app.route("/ns")
-def nuestros_servicios():
-    return render_template("ns.html")
 
 
 if __name__ == '__main__':
